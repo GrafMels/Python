016d5b1ab581eb6113acc603f9b22f4e3e668e33
Mels <ilia2577@gmail.com>
2022-10-17 19:47:58 +0800

    Seminar006: Homework: End

--- Seminar006/Домашняя работа/Program_003.py
+++ Seminar006/Домашняя работа/Program_003.py
@@ -1,7 +1,10 @@
 N = '0101010203030346034959595956989898987'
 
 list_non_repeating = []
-list_in_order = []
+
+list_in_order = [i for i in range(10)]
+
+list_in_order_num = []
 
 for i in range(10):
     count = 0
@@ -10,7 +13,13 @@ for i in range(10):
     for j in enumerate(N):
         if int(j[1]) == int(i):
             count +=1
+    list_in_order_num.append(count)
     if count == 1:
         list_non_repeating.append(i)
 
-print(list_non_repeating)
+glued_list = list(zip(list_in_order, list_in_order_num))
+
+for i in range(10):
+    print(f'Частота попаданий в {glued_list[i][0]}: {glued_list[i][1]} раз')
+
+print(f'Ответ: {list_non_repeating}')

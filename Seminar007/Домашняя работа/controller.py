7a53ea4102bedc04861f5b28e636a3adfc03beef
Mels <ilia2577@gmail.com>
2022-10-21 12:05:47 +0800

    Seminar007: Homework: End

--- Seminar007/Домашняя работа/controller.py
+++ Seminar007/Домашняя работа/controller.py
@@ -8,31 +8,51 @@ def input_string(enter):
 
 def fragmentation():
     start_string = model.full_string
-    model.full_string = str(model.full_string).replace('-',' - ').replace('+',' + ').replace('/',' / ')
+
+    
+    model.full_string = str(model.full_string).replace('-',' - ').replace('+',' + ')
     if(model.full_string[-1])=='*' or (model.full_string[-1])=='/' or (model.full_string[-1])=='+' or (model.full_string[-1])=='-':
         view.error_value()
     list_string = model.full_string.split()
 
     for i in range(len(list_string)):
-        if list_string[i].find('*') != -1:
+        if list_string[i].find('*') != -1 and list_string[i].find('/') != -1:
+            temp_str = list_string[i]
+            temp_list = temp_str.replace('*',' * ').replace('/',' / ').split()
+            temp_var = 0
+            count = 0
+            for j in range(len(temp_list)):
+                if temp_list[j] == '*' and count == 0:
+                    temp_var = int(temp_list[j-1]) * int(temp_list[j+1])
+                    count+=1
+                elif temp_list[j] == '/' and count == 0:
+                    temp_var = int(temp_list[j-1]) / int(temp_list[j+1])
+                    if temp_var == int(temp_var):
+                        temp_var = int(temp_var)
+                        count+=1
+                elif temp_list[j] == '*' and count != 0:
+                    temp_var *= int(temp_list[j+1])
+                elif temp_list[j] == '/' and count != 0:
+                    temp_var /= int(temp_list[j+1])
+                    if temp_var == int(temp_var):
+                        temp_var = int(temp_var)
+            print(temp_var)
+            list_string[i] = str(temp_var)    
+        elif list_string[i].find('*') != -1:
             temp_str = list_string[i]
             temp_list = temp_str.replace('*',' * ').split()
             temp_var = 0
-            for j in range(3):
+            for j in range(len(temp_list)):
                 if temp_list[j]=='*':
                     temp_var = int(temp_list[j-1]) * int(temp_list[j+1])
+            
             list_string[i] = str(temp_var)
             
-    model.full_string = ''.join(list_string)
-    model.full_string = model.full_string.replace('-',' - ').replace('+',' + ')
-    list_string = model.full_string.split()
-    
-    for i in range(len(list_string)):
-        if list_string[i].find('/') != -1:
+        elif list_string[i].find('/') != -1:
             temp_str = list_string[i]
             temp_list = temp_str.replace('/',' / ').split()
             temp_var = 0
-            for j in range(3):
+            for j in range(len(temp_list)):
                 if temp_list[j]=='/':
                     if int(temp_list[j+1]) == 0:
                         view.dev_by_zero()
@@ -40,7 +60,7 @@ def fragmentation():
                     temp_var = int(temp_list[j-1]) / int(temp_list[j+1])
                     if temp_var == int(temp_var):
                         temp_var = int(temp_var)
-            list_string[i] = str(temp_var)
+            list_string[i] = str(temp_var)      
                      
     model.full_string = ''.join(list_string)
     model.full_string = model.full_string.replace('-',' -').replace('+',' +')
@@ -48,7 +68,7 @@ def fragmentation():
     
     temp_var = 0
     for i in range(len(list_string)):
-        temp_var += int(list_string[i])
+        temp_var += float(list_string[i])
     
     logger.logger(f'{start_string}={temp_var}')
     model.full_string = temp_var
